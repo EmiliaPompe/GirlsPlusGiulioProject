aa778f1fa4f70811197e4579b82a23f20e9aa889
Tamar Loach <tamar.loach@spc.ox.ac.uk>
2017-02-08 09:23:39 +0000

    post adjustment error plots

--- post_adjustment.py
+++ post_adjustment.py
@@ -14,12 +14,18 @@ def EpanechnikovKernel(t,delta,c=1):
         return 0
 
 def PostProcess(abc_df, Summary, data, q=0.5, weighted=True):
-    print "here"
     """
     Given a dataframe output to ABC, return dataframe with accepted thetas transformed in column "theta_star"
     """
     # take accpted values output from ABC
     df_accepted = abc_df[abc_df['accept'] == 1]
+    accepted_count = len(df_accepted.index)
+    if accepted_count < 5:
+    	print accepted_count, "is number of accepted thetas"
+
+    if accepted_count < 2:
+    	print "Post processing failed; too few accepted values."
+    	return 0
 
     df_accepted.statistics_diff = df_accepted.statistics - Summary(data)
     df_accepted.statistics_diff_abs = df_accepted.statistics_diff.apply(lambda x: abs(x))
@@ -46,13 +52,11 @@ def PostProcess(abc_df, Summary, data, q=0.5, weighted=True):
         res = mod.fit(X, y)
     #alpha = res.intercept_[0]
     beta = res.coef_[0][0]
-    print beta
     beta_stats_diff = beta*df_accepted.statistics_diff
     beta_stats_diff_array = np.array(beta_stats_diff)
     beta_stats_diff_array.shape = (shape(beta_stats_diff_array)[0],1)
     theta_star = np.subtract(y, beta_stats_diff_array)
     theta_star = np.reshape(theta_star, theta_star.shape[0])
-    print np.shape(theta_star)
     df_accepted['theta_star'] = theta_star
     
     return df_accepted
